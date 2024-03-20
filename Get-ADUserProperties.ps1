3c5154a08bf1a7a9a9895cd5249658e545d2c6e8
CarnageCode <harrylockheed@protonmail.com>
2024-03-20 18:41:16 -0500

    Carnage: Generic script to get AD user properties from csv

---
+++ Get-ADUserProperties.ps1
@@ -0,0 +1,16 @@
+﻿<#
+.SYNOPSIS
+Provide a filename for a csv and the script will look for the username header of it, check Active Directory for the usernames, and return properties such as GivenName, Enabled, MemberOf, and CreatedTimeStamp.
+.PARAMETER csvname
+.EXAMPLE
+Get-ADUserProperties -csvname user.csv 
+#>
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$csvname
+    )
+$UserList = (Import-CSV $csvname)
+Foreach($User in $UserList) {
+$GivenName = $User.Username
+Get-ADUser -Filter 'Name -eq $GivenName' -Properties "MemberOf", "CreateTimeStamp", "modifyTimeStamp" | Format-List -Property GivenName,Enabled,MemberOf,modifyTimeStamp,CreateTimeStamp
+}
